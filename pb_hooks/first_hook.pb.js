d97386abfe508e768479dc5b89f4399defa15b26
henriqueos98 <henriqueos98@outlook.com>
2024-12-19 17:53:57 -0300

    OK

--- pb_hooks/first_hook.pb.js
+++ pb_hooks/first_hook.pb.js
@@ -10,6 +10,4 @@ onRecordCreateRequest((e) => {
 onRecordUpdateRequest((e) => {
     console.log("Hello, World!");
     return e.next();
-});create a script that thakes data from the csvs in cms_cost_reports extracted, look for files that ends in  _rpt.csv and parse the ocntent to that table 
-
-CREATE OR REPLACE TABLE CMS_RAW.SNF.COSTREPORT_SNF_FISCAL_YEAR (
+});
